3b753cdd51e0847eb6e401706a2e2ff2bf1e1687
isibolivia2019 <isibolivia2019@gmail.com>
2019-03-16 10:23:03 -0400

    subida nro. 1

---
+++ reporte-inventario.php
@@ -0,0 +1,130 @@
+<?php
+ini_set('display_errors', '1');
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <?php require("app-head.php");?>
+    </head>
+
+    <body>
+        <div id="loader-wrapper">
+            <div id="loader"></div>        
+            <div class="loader-section section-left"></div>
+            <div class="loader-section section-right"></div>
+        </div>
+
+        <div id="loader-wrapper">
+            <div id="loader"></div>        
+            <div class="loader-section section-left"></div>
+            <div class="loader-section section-right"></div>
+        </div>
+
+        <?php require("app-header.php");?>
+        
+        <div id="main">
+            <div class="wrapper">
+                <?php require("app-slider.php");?>
+                
+                <section id="content">
+                    <div id="breadcrumbs-wrapper" class=" grey lighten-3">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col s12 m12 l12">
+                                    <h5 class="breadcrumbs-title">Reportes de Inventario</h5>
+                                    <ol class="breadcrumb">
+                                        <li><a href="inicio.php">Inicio</a></li>
+                                        <li class="active">Inventario de Productos</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container">
+                        <div class="section">
+                            <div class="divider"></div>
+                            <div class="row">
+                                <form class="col s12">
+                                <h4 class="header">Seleccione el Punto de Almacenamiento</h4>
+                                    <div class="row">
+                                        <div class="col s12 m12 l4">
+                                            <label>Almacenamiento</label>
+                                            <select class="browser-default" id="cboxAlmacenamiento">
+                                                <option value="" disabled selected>Seleccione la Sucursal</option>
+                                            </select>
+                                        </div>
+                                        <div class="row">
+                                            <div class="input-field col s12 submitBtn">
+                                                <div class="input-field col s12 right ">
+                                                  <a class="btn waves-effect waves-light col s12" onclick="generarReporte()">Generar Reporte de Compras</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <br>
+                            <div class="divider"></div> 
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+        <?php require("app-footer.php");?>
+        <?php require("app-foot.php");?>
+
+    <script>
+        $(document).ready(function() {
+            verificarAcceso("Permiso_Reporte");
+            var cboxAlmacenamiento = document.getElementById("cboxAlmacenamiento");
+            var parametros = {
+            "action" : "listaAlmacenes"            
+          };
+          $.ajax({
+            type:'POST',
+            data: parametros,
+            url:'app/controladores/Almacenes.php',
+            success:function(data){
+                datos = JSON.parse(data);
+                datos = datos.data;
+                for(let i=0 ; i<datos.length ; i++){
+                    var tag = document.createElement('option');
+                    tag.setAttribute('value', datos[i].cod_almacen);
+                    tag.innerHTML = datos[i].nombre_almacen;
+                    cboxAlmacenamiento.appendChild(tag);
+                }
+            }
+          })
+          parametros = {
+             "action" : "listaSucursales"      
+          };
+          $.ajax({
+            type:'POST',
+            data: parametros,
+            url:'app/controladores/Sucursales.php',
+            success:function(data){
+                datos = JSON.parse(data);
+                datos = datos.data;
+                for(let i=0 ; i<datos.length ; i++){
+                    var tag = document.createElement('option');
+                    tag.setAttribute('value', datos[i].cod_sucursal);
+                    tag.innerHTML = datos[i].nombre_sucursal;
+                    cboxAlmacenamiento.appendChild(tag);
+                }
+            }
+          })
+            
+        });
+
+        function generarReporte(){
+            verificarAcceso("Permiso_Reporte");
+            var cboxAlmacenamiento = document.getElementById("cboxAlmacenamiento").value;
+            window.open("reportes/reporte-inventario.php?suc="+cboxAlmacenamiento,'New Window'); 
+        }
+        
+    </script>
+</body>
+</html>
